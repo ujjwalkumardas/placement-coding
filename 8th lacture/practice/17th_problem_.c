1f49aad78ce8a1146a624b5ac61b28911d8ef31f
ujjwal kumar <129957718+ujjwalkumardas@users.noreply.github.com>
2025-03-25 22:13:11 +0530

    new

--- 8th lacture/practice/17th_problem_.c
+++ 8th lacture/practice/17th_problem_.c
@@ -6,5 +6,22 @@ SAMPLE INPUT :-  1100
 SAMPLE OUTPUT:-1010   */
 #include<stdio.h>
 int main(){
-    
+    int bit;
+    scanf("%d",&bit);
+    int binary[bit];
+    int gray[bit];
+    for(int i=0;i<bit;i++){
+        int n;
+        scanf("%d",&n);
+        binary[i]=n;
+
+    }
+  gray[0]=binary[0];
+  int graydata;
+    for(int i=1;i<bit;i++){
+        graydata=binary[i-1]^binary[i];
+        gray[i]=graydata;
+    }
+    for(int i=0;i<bit;i++){
+    printf("%d",gray[i]);}
 }
