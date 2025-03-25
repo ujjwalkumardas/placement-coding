68f40febfd4c57228afd118cbd284bfa2fef730d
ujjwal kumar <129957718+ujjwalkumardas@users.noreply.github.com>
2025-03-25 23:03:09 +0530

    new

---
+++ 9th lacture/practice/19th_problem_.c
@@ -0,0 +1,14 @@
+/*PROBLEM DEFINATION :-  Write a C program for given two numbers N and K, our task is to subtract a number K
+from N until number(N) is greater than zero, once the N becomes negative or zero then we start adding K
+until that number become the original number(N).
+
+CONSTRAINTS:- n<10^9 and k<10^3
+INPUT FORMAT :- integer
+OUTPUT FORMAT:-result sum of the series
+SAMPLE INPUT :- N=15,K=5
+SAMPLE OUTPUT:- 15 10 5 0 5 10 15 
+-----------------------------------------*/
+#include<stdio.h>
+int main(){
+  
+}
