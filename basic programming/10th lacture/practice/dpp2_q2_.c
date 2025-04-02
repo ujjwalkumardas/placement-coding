1913be05ef51d89863ead5a7695308775ee504f6
ujjwal kumar <129957718+ujjwalkumardas@users.noreply.github.com>
2025-04-02 20:07:26 +0530

    update

---
+++ basic programming/10th lacture/practice/dpp2_q2_.c
@@ -0,0 +1,20 @@
+/*#Problem - 2
+PROBLEM DEFINATION :-  A positive integer is called an Armstrong number (of order n) if
+abcd ... = a^n + b^n + c^n + d^n + ...
+In the case of an Armstrong number of 3 digits, the sum of cubes of each digit is equal to the number itself.
+For example, 153 is an Armstrong number because 153 = 1*1*1 + 5*5*5 + 3*3*3. Write 'C' Code that find the
+given number is Armstrong number or not.
+CONSTRAINTS:- N <= 106
+INPUT FORMAT :-  A Positive Integer
+OUTPUT FORMAT:-  Prints Armstrong or not
+Sample Input: 1634
+Sample Output: 1634 is Armstrong Number
+
+****************************************************************************************************************************/
+
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+}
