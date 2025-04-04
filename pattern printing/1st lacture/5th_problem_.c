33a02b00bc7ce2b59b5ed4225f2c719c66be5c05
ujjwal kumar <129957718+ujjwalkumardas@users.noreply.github.com>
2025-04-04 22:06:33 +0530

    folder update

--- pattern printing/1st lacture/5th_problem_.c
+++ pattern printing/1st lacture/5th_problem_.c
@@ -7,19 +7,22 @@ Sample Input1: 4 5
 Sample Output1:
 
 */
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
     char ch;
     int row;
     int col;
     printf("inter symbol = ");
-    scanf("%c",&ch);
+    scanf("%c", &ch);
     printf("enter row & column = ");
-    scanf("%d%d",&row,&col);
-    for(int i=0;i<row;i++){
+    scanf("%d%d", &row, &col);
+    for (int i = 0; i < row; i++)
+    {
         printf("\n");
-        for(int j=0;j<=i;j++){
-            printf("%c  ",ch+j);
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%c  ", ch + j);
         }
     }
 }
